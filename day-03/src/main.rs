dd4f9bc45e9db6386d822127b44452ecf99c6686
Walther <veeti.haapsamo@gmail.com>
2021-12-03 18:50:50 +0200

    Day 03: tiny stylistic improvements, more comments

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -4,14 +4,13 @@ fn main() {
     const INPUT: &str = include_str!("input.txt");
     let lines = INPUT.lines();
 
-    // parse into a 2d vec
+    // Parsing phase. Parse into a 2D array of bits using a Vec of BitVecs
+    // TODO: cleanup
     let mut table: Vec<BitVec> = Vec::new();
     for line in lines {
         let mut row = bitvec![];
-
         for bit in line.chars() {
             let value: u32 = bit.to_string().parse().unwrap();
-            // TODO: cleanup
             let value = match value {
                 0 => false,
                 1 => true,
@@ -21,9 +20,9 @@ fn main() {
         }
         table.push(row);
     }
-
     let width = table[0].len();
 
+    // Construct the values by collecting bits from the columns
     let mut gamma_bits = bitvec![];
     let mut epsilon_bits = bitvec![];
 
@@ -31,21 +30,26 @@ fn main() {
         gamma_bits.push(most_common_bit_in_column(&table, column));
         epsilon_bits.push(least_common_bit_in_column(&table, column));
     }
-    // bit order shenanigans. // TODO: cleanup!
+
+    // Bit order shenanigans. // TODO: cleanup!
     gamma_bits.reverse();
     epsilon_bits.reverse();
 
+    // Convert bits into usize
     let gamma_rate = gamma_bits.into_vec()[0];
     let epsilon_rate = epsilon_bits.into_vec()[0];
 
+    // Compute and print the result of Part 1
     let power_consumption = gamma_rate * epsilon_rate;
     println!("Part 1: {}", power_consumption);
 
-    // iteratively filter through the tables to reduce the numbers into one rating
+    // Re-use the original data for new purposes
     let mut oxygen_rating = table.clone();
     let mut co2_rating = table.clone();
 
-    for column in 0..oxygen_rating[0].len() {
+    // Iteratively filter through the tables to reduce the rows into one final row to keep
+
+    for column in 0..width {
         let most_common = most_common_bit_in_column(&oxygen_rating, column);
         oxygen_rating.retain(|x| x[column] == most_common);
         if oxygen_rating.len() == 1 {
@@ -53,7 +57,7 @@ fn main() {
         }
     }
 
-    for column in 0..co2_rating[0].len() {
+    for column in 0..width {
         let least_common = least_common_bit_in_column(&co2_rating, column);
         co2_rating.retain(|x| x[column] == least_common);
         if co2_rating.len() == 1 {
@@ -61,15 +65,19 @@ fn main() {
         }
     }
 
+    // Get the bits of the final remaining rows // TODO: can this be simplified?
     let mut oxygen_bits = oxygen_rating[0].clone();
     let mut co2_bits = co2_rating[0].clone();
-    // bit order shenanigans. // TODO: cleanup!
+
+    // Bit order shenanigans. // TODO: cleanup!
     oxygen_bits.reverse();
     co2_bits.reverse();
 
+    // Convert bits into usize
     let oxygen_rating = oxygen_bits.into_vec()[0];
     let co2_rating = co2_bits.into_vec()[0];
 
+    // Compute and print the result of Part 2
     println!("Part 2: {}", oxygen_rating * co2_rating);
 }
 
@@ -85,6 +93,5 @@ fn most_common_bit_in_column(table: &[BitVec], column: usize) -> bool {
 
 /// Given a 2D table of bits and a column index, returns the least common bit of that column. If there's an equal amount of 0s and 1s, 0 is returned. This mirrors the behavior of [most_common_bit_in_column].
 fn least_common_bit_in_column(table: &[BitVec], column: usize) -> bool {
-    let most_common = most_common_bit_in_column(table, column);
-    !most_common
+    !most_common_bit_in_column(table, column)
 }
