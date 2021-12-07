a06d24372c05360754ec0d3f15c77b0d43385990
Walther <veeti.haapsamo@gmail.com>
2021-12-07 15:01:06 +0200

    Refactor day 02: simplify

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Context, Error, Result};
 use std::str::FromStr;
+use Instruction::{Down, Forward, Up};
 
 fn main() -> Result<()> {
     const INPUT: &str = include_str!("input.txt");
@@ -7,7 +8,7 @@ fn main() -> Result<()> {
 
     // We have two travel modes for the same instructions:
     // - simple, literal travel based on the instruction direction and offset
-    // - aim, where up and down instructions change the tilt of the submarine, and only forward instruction is used for travel
+    // - aim, where up and down instructions change the tilt of the submarine, and forward instruction propels forward
     // We go through the instruction list once and track our travel based on both methods at the same time.
     let mut simple_coords: Vec<(u32, u32)> = Vec::new();
     let mut aim_coords: Vec<(u32, u32)> = Vec::new();
@@ -20,18 +21,20 @@ fn main() -> Result<()> {
     for line in lines {
         let instruction: Instruction = line.parse()?;
 
-        // NOTE: Submarine coordinates: origin is (0,0), y down is positive
+        // NOTE: Submarine coordinates:
+        // - origin is (0,0) at the surface
+        // - y down is positive
         match instruction {
-            Instruction::Forward(offset) => {
+            Forward(offset) => {
                 simple_position.0 += offset;
                 aim_position.0 += offset;
                 aim_position.1 += aim * offset;
             }
-            Instruction::Down(offset) => {
+            Down(offset) => {
                 simple_position.1 += offset;
                 aim += offset;
             }
-            Instruction::Up(offset) => {
+            Up(offset) => {
                 simple_position.1 -= offset;
                 aim -= offset;
             }
@@ -40,7 +43,7 @@ fn main() -> Result<()> {
         aim_coords.push(aim_position);
     }
 
-    // final x and y coordinates multiplied
+    // Print the solutions: final x and y coordinates multiplied
     let simple_final = simple_coords.last().unwrap();
     println!("Part 1: {}", simple_final.0 * simple_final.1);
     let aim_final = aim_coords.last().unwrap();
@@ -64,9 +67,9 @@ impl FromStr for Instruction {
         let offset: u32 = offset.parse()?;
 
         match direction {
-            "forward" => Ok(Instruction::Forward(offset)),
-            "down" => Ok(Instruction::Down(offset)),
-            "up" => Ok(Instruction::Up(offset)),
+            "forward" => Ok(Forward(offset)),
+            "down" => Ok(Down(offset)),
+            "up" => Ok(Up(offset)),
             _ => Err(anyhow!("Invalid direction")),
         }
     }
